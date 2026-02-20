11e972f643b25515889f12d9c90ad617980c3127
Marta Taberner <marta.taberner@estudiantat.upc.edu>
2026-02-20 20:09:50 +0100

    He afegit les classes de les àrees de les figures

--- src/Shape.java
+++ src/Shape.java
@@ -1,3 +1,51 @@
-public class Shape {
+public abstract class Shape {
     public abstract double area ();
 }
+// Rectangle
+class Rectangle extends Shape {
+    double l1, l2;
+
+    public Rectangle(double l1, double l2) {
+        this.l1 = l1;
+        this.l2 = l2;
+    }
+
+    public double area() {
+        return l1 * l2;
+    }
+}
+
+// Square
+class Square extends Rectangle {
+    public Square(double l) {
+        super(l, l);
+    }
+}
+
+// Triangle
+class Triangle extends Shape {
+    double b, h;
+
+    public Triangle(double b, double h) {
+        this.b = b;
+        this.h = h;
+    }
+
+    public double area() {
+        return (b * h) / 2;
+    }
+}
+
+class Circle extends Shape {
+    double r;
+
+    public Circle(double r) {
+        this.r = r;
+    }
+
+    public double area() {
+        return Math.PI * r * r;
+    }
+}
+
+
